929f62e4a82f709b8b2b1c0c7f3b2e487c097258
Holger Imbery <contact@connectedobjects.cloud>
2018-07-15 14:59:24 +0200

    Initial commit

---
+++ generate_img.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# script to generate the CCU addon package.
+
+# generate tempdir
+mkdir -p tmp/
+rm -rf tmp/*
+
+# copy all relevant stuff
+cp -a update_script tmp/
+cp -a VERSION tmp/
+cp -a www tmp/
+cp -a rc.d tmp/
+cp -a profile.d tmp/
+cp -a update_addon tmp/
+cp -a *.cfg tmp/
+
+# generate archive
+cd tmp
+tar --exclude=.DS_Store -czvf ../feeder-button-$(cat ../VERSION).tar.gz *
+#tar --owner=root --group=root --exclude=.DS_Store -czvf ../feeder-button-$(cat ../VERSION).tar.gz *
+cd ..
+rm -rf tmp
---
+++ profile.d/feeder-button-addons.sh
@@ -0,0 +1,2 @@
+export PATH=${PATH}:/usr/local/addons/feeder-button/bin
+export TERM=xterm
